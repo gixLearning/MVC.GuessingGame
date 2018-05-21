08958e495aedb0de4fd560a8027e919e38aa8e05
gixLearning <38102077+gixLearning@users.noreply.github.com>
2018-05-21 15:40:28 +0200

    Initial commit

---
+++ MVC.GuessingGame/App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MVC.GuessingGame
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Fevercheck",
+                url: "fevercheck/",
+                defaults: new { controller = "Page", action = "Fevercheck" }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Page", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ MVC.GuessingGame/Controllers/PageController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.GuessingGame.Controllers
+{
+    public class PageController : Controller
+    {
+        // GET: Page
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About() {
+            return View();
+        }
+
+        public ActionResult Contact() {
+            return View();
+        }
+
+        public ActionResult Projects() {
+            return View();
+        }
+
+        public ActionResult Fevercheck() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult FeverCheck(float? temperature, string useFarenheit) {
+            if (temperature != null) {
+                ViewData["temps"] = temperature;
+
+                if (!useFarenheit.Equals("C")) {
+                    ViewData["useFarenheit"] = true;
+                } else {
+                    ViewData["useFarenheit"] = false;
+                }
+
+                return View();
+            }
+
+            return View();
+        }
+    }
+}
